4e3b93a24beb4060c133b64de7a7bdb6f9845cba
824775562@qq.com <sun8562778>
2021-05-14 16:09:09 +0800

    换成lua脚本，保证公平性

--- sk-preorder/src/main/java/com/siwen/seckilling/service/impl/SecKillingServiceImpl.java
+++ sk-preorder/src/main/java/com/siwen/seckilling/service/impl/SecKillingServiceImpl.java
@@ -1,9 +1,7 @@
 package com.siwen.seckilling.service.impl;
 
 import com.siwen.domain.User;
-import com.siwen.seckilling.bean.PreOrder;
 import com.siwen.seckilling.constant.ResultStatus;
-import com.siwen.seckilling.service.GoodsService;
 import com.siwen.seckilling.service.PreOrderService;
 import com.siwen.seckilling.service.SecKillingService;
 import com.siwen.seckilling.vo.Result;
@@ -12,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Objects;
 
 /**
  * @Author siwen
@@ -26,9 +23,6 @@ public class SecKillingServiceImpl implements SecKillingService {
     @Resource
     private PreOrderService preOrderService;
 
-    @Resource
-    private GoodsService goodsService;
-
 
     @Override
     public Result<Boolean> doSecKilling(User user, String goodsId) {
@@ -38,14 +32,13 @@ public class SecKillingServiceImpl implements SecKillingService {
         //      1: 表示抢成功了
         //      2：表示已经抢过了
         if (0 == flag) {
-            logger.info(goodsId + " 抢光了");
+            logger.info(goodsId + "秒杀结束");
             result = Result.build(ResultStatus.SALE_OVER);
         } else if (1 == flag) {
-            logger.info(user.getId() + " 抢到了");
+            logger.info(user.getId() + "秒杀成功");
             result = Result.buildSuccess();
             result.setData(true);
         } else if (2 == flag) {
-            logger.info(user.getId() + " 重复抢了");
             result = Result.build(ResultStatus.REPEAT_SEC_KILLING);
         } else {
             result = Result.build(ResultStatus.EXCEPTION);
